3d5ea939501f0aab2f24992de21d39612c1fecc2
Petibros <snap33640@gmail.com>
2025-01-17 23:00:48 +0100

    new parsing potentiallt faster

--- src/parsing.c
+++ src/parsing.c
@@ -21,9 +21,9 @@ static int	count_numbers(char *line)
 	n = 0;
 	while (line[i])
 	{
-		while (line[i] && (line[i] == ' ' || line[i] == '\n'))
+		while (line[i] && line[i] == ' ')
 			++i;
-		if (!line[i])
+		if (!line[i] || line[i] == '\n')
 			break ;
 		++n;
 		if (line[i] == '-' || line[i] == '+')
@@ -48,9 +48,9 @@ static void	put_in_tab(char *line, t_args *args, int *tab)
 	n = 0;
 	while (line[i])
 	{
-		while (line[i] && (line[i] == ' ' || line[i] == '\n'))
+		while (line[i] && line[i] == ' ')
 			++i;
-		if (!line[i])
+		if (!line[i] || line[i] == '\n')
 			break ;
 		tab[n] = ft_atoi(&line[i]);
 		++n;
@@ -67,49 +67,65 @@ static void	put_in_tab(char *line, t_args *args, int *tab)
 	args->size_x = n;
 }
 
-static void	read_map(t_args *args)
+static void	fill_tab(t_args *args, char *file)
 {
-	char	*line;
 	int		i;
-
-	i = 0;
-	line = get_next_line(args->fd);
-	while (1)
-	{
-		args->map[i] = malloc(count_numbers(line) * sizeof(int));
-		if (!args->map[i])
-			exit_msg(args, "Failed to alloc tab", 1, 1);
-		put_in_tab(line, args, args->map[i]);
-		free(line);
-		line = get_next_line(args->fd);
-		if (!line)
-			break ;
-		++i;
-	}
-}
-
-int	count_line(t_args *args, char *file)
-{
-	char	*line;
 	int		n;
 
 	n = 0;
-	line = get_next_line(args->fd);
-	while (line)
+	i = 0;
+	while (file[i])
 	{
-		free(line);
-		line = get_next_line(args->fd);
+		args->map[n] = malloc(count_numbers(&file[i]) * sizeof(int));
+		if (!args->map[n])
+			exit_msg(args, "Failed to alloc tab", 1, 1);
+		put_in_tab(&file[i], args, args->map[n]);
+		while (file[i] && file[i] != '\n')
+			++i;
+		if (file[i] == '\n')
+			++i;
 		++n;
 	}
-	close(args->fd);
-	args->fd = open(file, O_RDONLY);
-	args->size_y = n;
-	return (n);
+	free(file);
+}
+
+static char	*read_map(t_args *args)
+{
+	int		i;
+	char	buffer[4096];
+	char	*file;
+	char	*tmp;
+	int		n_read;
+
+	i = 0;
+	file = ft_calloc(1, sizeof(char));
+	n_read = read(args->fd, buffer, 4095);
+	while (n_read)
+	{
+		buffer[n_read] = 0;
+		tmp = ft_strdup(file);
+		if (!tmp || !file)
+			exit_msg(args, "Malloc error whilst reading map", 1, 1);
+		free(file);
+		file = ft_strjoin(tmp, buffer);
+		free(tmp);
+		n_read = read(args->fd, buffer, 4095);
+	}
+	while (file[i])
+	{
+		if (file[i] == '\n')
+			++args->size_y;
+		++i;
+	}
+	if (i >= 1 && file[i - 1] != '\n')
+		++args->size_y;
+	return (file);
 }
 
 t_args	*parsing(char **argv)
 {
 	t_args	*args;
+	char	*file;
 
 	args = malloc(sizeof(t_args));
 	if (!args)
@@ -117,9 +133,11 @@ t_args	*parsing(char **argv)
 	args->fd = open(argv[1], O_RDONLY);
 	if (args->fd == -1)
 		exit_msg(args, "Invalid file", 0, 1);
-	args->map = malloc(count_line(args, argv[1]) * sizeof(int *));
+	args->size_y = 0;
+	file = read_map(args);
+	args->map = malloc(args->size_y * sizeof(int *));
 	if (!args->map)
 		exit_msg(args, "Failed to alloc map", 1, 1);
-	read_map(args);
+	fill_tab(args, file);
 	return (args);
 }
